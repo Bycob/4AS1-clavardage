f8282c671d05b531a902d8b9b35d6a7bce1c3f35
Stefan Nesic <stefan.nesic@inria.fr>
2018-12-11 14:57:46 +0100

    Define Message

--- src/org/ljsn/clavardage/core/Message.java
+++ src/org/ljsn/clavardage/core/Message.java
@@ -1,5 +1,26 @@
 package org.ljsn.clavardage.core;
 
 public class Message {
-
+    private Date timestamp;
+    private String content;
+    private User author;
+    
+    public Message(Date timestamp, String content, User author) {
+    	this.timestamp = timestamp;
+    	this.content = content;
+    	this.author = author;
+    	return this;
+    }
+    
+    public getTime() {
+    	return this.timestamp;
+    }
+    
+    public getContent() {
+    	return this.content;
+    }
+    
+    public getAuthor() {
+    	return this.author;
+    } 
 }
