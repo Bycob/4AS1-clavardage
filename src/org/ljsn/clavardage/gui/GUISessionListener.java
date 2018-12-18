808fc636388f0ef17f653479534a73a2f21c6768
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2018-12-18 23:56:29 +0100

    GUISessionListener can now execute arbitrary code on JavaFX thread easily

--- src/org/ljsn/clavardage/gui/GUISessionListener.java
+++ src/org/ljsn/clavardage/gui/GUISessionListener.java
@@ -1,10 +1,18 @@
 package org.ljsn.clavardage.gui;
 
+import java.util.LinkedList;
+
 import org.ljsn.clavardage.core.SessionListener;
+
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 
 public class GUISessionListener implements SessionListener {
 
 	private ClavardageApp app;
+	
+	private LinkedList<Task> taskQueue = new LinkedList<Task>();
 	
 	public GUISessionListener(ClavardageApp app) {
 		this.app = app;
@@ -12,7 +20,12 @@ public class GUISessionListener implements SessionListener {
 	
 	@Override
 	public void onConnectionFailed(Exception error) {
-		
+		runTask(new ExecOnJavaFXThread() {
+			@Override
+			public void handle(WorkerStateEvent event) {
+				System.out.println(Thread.currentThread().getName());
+			}
+		});
 	}
 
 	@Override
@@ -23,5 +36,23 @@ public class GUISessionListener implements SessionListener {
 	@Override
 	public void onMessageSent() {
 		
+	}
+	
+	private void runTask(Task task) {
+		Thread th = new Thread(task);
+		th.setDaemon(true);
+		th.start();
+	}
+	
+	private abstract class ExecOnJavaFXThread extends Task<Boolean> implements EventHandler<WorkerStateEvent> {
+
+		public ExecOnJavaFXThread() {
+			setOnSucceeded(this);
+		}
+		
+		@Override
+		protected Boolean call() {
+			return true;
+		}
 	}
 }
